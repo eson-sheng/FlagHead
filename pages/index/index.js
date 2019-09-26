b4ef9b43bc9ad89a6728901d31a93cee562e92a7
eson <834767372@qq.com>
2019-09-26 19:50:41 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,122 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: '',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    picUrl: '',
+  },
+  //事件处理函数
+  onLoad: function () {
+    this.getPicUrl();
+    console.log(this.data.picUrl);
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+
+    if (e.detail.rawData) {
+      wx.login({
+        success(res) {
+          if (res.code) {
+            // 发起网络请求
+            console.log(res);
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
+        }
+      })
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+    }
+    this.getPicUrl();
+
+  },
+  getPicUrl: function () {
+    let self = this;
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              wx.request({
+                url: 'https://wx.eson.site/', //仅为示例，并非真实的接口地址
+                method: 'POST',
+                data: res.userInfo,
+                header: {
+                  'content-type': 'application/json' // 默认值
+                },
+                success: function (res) {
+                  console.log(res.data)
+                  self.setData({
+                    picUrl: res.data.pic,
+                  })
+                  self.data.picUrl = res.data.pic;
+                }
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+  downfile: function () {
+    let self = this;
+    wx.downloadFile({
+      url: self.data.picUrl,
+      success: function (res) {
+        console.log(res)
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+          success: function (res) {
+            wx.showToast({
+              title: '保存成功',
+              icon: 'success',
+              duration: 2000
+            })
+            console.log(res)
+          },
+          fail: function (res) {
+            console.log(res)
+            console.log('fail')
+          }
+        })
+      },
+      fail: function () {
+        console.log('fail')
+      }
+    })
+  }
+})
